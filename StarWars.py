da6a7f41384bbf4ac9b78e841df4dc97389200a7
Fede1979 <fedesanchez@gmail.com>
2025-05-20 19:31:24 -0300

    correccion de codigo

--- StarWars.py
+++ StarWars.py
@@ -1,9 +1,9 @@
-    # Pre-Entrega
+# Pre-Entrega
 # Ejercicio 1: Crear un programa que permita agregar productos a una lista y mostrarlos en pantalla.
 productos = []
 
 while True:
-    cartel_bienvenida = "bienvenido a la vinoteca".upper()
+    cartel_bienvenida = "bienvenido a la tienda de star wars".upper()
     print(cartel_bienvenida)
     print("1. Agregar producto")
     print("2. Mostrar productos")
@@ -13,22 +13,40 @@ while True:
     opcion = int(input("Seleccione una opción: "))
 
     if opcion == 1:
-        nombre_producto = input("Ingrese el nombre del producto: ")
-        precio_producto = int(input("Ingrese el precio del producto: "))
-        cantidad_producto = int(input("Ingrese la cantidad del producto: "))
-        productos.append({"nombre": nombre_producto.lower(), "precio": precio_producto, "cantidad": cantidad_producto})
+        while True:
+            nombre_producto = input("Ingrese el nombre del producto: ").strip()
+            if nombre_producto == "":
+                print("Debe agregar un producto.")
+            else:
+                break
+        while True:
+            cat_producto = (input("Ingrese su categoria: "))
+            if cat_producto == "":
+                print("Debe agregar una categoria.")
+            else:
+                break
+        while True:
+            precio_producto = (input("Ingrese su precio sin centavos: "))
+            if precio_producto == "":
+                print("Debe agregar un precio sin centavos.")
+            else:
+                break              
+        productos.append({"nombre": nombre_producto.lower(), "categoria": cat_producto, "precio": precio_producto})
+        lista_productos = productos
+        
+        print(f"El producto {nombre_producto.upper()}, de la categoria {cat_producto.upper()} fue agregado correctamente y su valor es de ${precio_producto}")
     elif opcion == 2:
         if not productos:
             print("No hay productos en la tienda.")
         else:
             for producto in productos:
-                print(f"Nombre: {producto['nombre']}, Precio: {producto['precio']}, Cantidad: {producto['cantidad']}")
+                print(f"Nombre: {producto['nombre'].upper()}, categoria: {producto['categoria'].upper()}, precio: {producto['precio']}")
     elif opcion == 3:
         nombre_buscar = input("Ingrese el nombre del producto a buscar: ")
         encontrado = False
         for producto in productos:
             if producto['nombre'] == nombre_buscar.lower():
-                print(f"Producto encontrado: Nombre: {producto['nombre']}, Precio: {producto['precio']}, Cantidad: {producto['cantidad']}")
+                print(f"Producto encontrado: Nombre: {producto['nombre'].upper()}, categoria: {producto['categoria'].upper()}, precio: {producto['precio']}")
                 encontrado = True
                 break
         if not encontrado:
